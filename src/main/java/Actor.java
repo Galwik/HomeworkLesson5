d0b2a8a5536d0b10cd9a9be096482646a90eb365
galwik <gawlickikamil@gmail.com>
2022-03-22 15:51:43 +0100

    Added abstract class Person / Director and Actor extends Person

--- src/main/java/Actor.java
+++ src/main/java/Actor.java
@@ -1,33 +1,9 @@
-public class Actor {
-    private String firstName;
-    private String lastName;
+public class Actor extends Person{
 
     public Actor(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        super(firstName, lastName);
     }
 
     public Actor() {
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return firstName + " " + lastName;
     }
 }
--- src/main/java/Director.java
+++ src/main/java/Director.java
@@ -1,20 +1,2 @@
-public class Director {
-    private String firstName;
-    private String lastName;
-
-    public Director() {
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return firstName + " " + lastName;
-    }
+public class Director extends Person{
 }
